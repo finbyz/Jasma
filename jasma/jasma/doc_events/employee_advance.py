7ecc7f754e8006994c6c447ebce65a42a855b1b5
Ritik Patwa <ritik.patwa@finbyz.tech>
2026-07-21 09:12:49 +0000

    Employee Advance Payment Entry changes by PD

--- jasma/jasma/doc_events/employee_advance.py
+++ jasma/jasma/doc_events/employee_advance.py
@@ -2,7 +2,9 @@ import frappe
 from frappe import _
 from frappe.utils import nowdate
 
+
 def create_payment_entry(doc, method=None):
+    # Prevent duplicate Payment Entry
     if frappe.db.exists(
         "Payment Entry Reference",
         {
@@ -12,9 +14,30 @@ def create_payment_entry(doc, method=None):
     ):
         return
 
-    company = frappe.get_doc("Company", doc.company)
+    # Get Mode of Payment Account for the company
+    bank_account = frappe.db.get_value(
+        "Mode of Payment Account",
+        {
+            "parent": doc.mode_of_payment,
+            "company": doc.company,
+        },
+        "default_account",
+    )
 
+    if not bank_account:
+        frappe.throw(
+            _(
+                "Default account is not configured for Mode of Payment {0} "
+                "for Company {1}"
+            ).format(
+                frappe.bold(doc.mode_of_payment),
+                frappe.bold(doc.company),
+            )
+        )
+
+    # Create Payment Entry
     pe = frappe.new_doc("Payment Entry")
+
     pe.payment_type = "Pay"
     pe.company = doc.company
     pe.posting_date = nowdate()
@@ -22,32 +45,35 @@ def create_payment_entry(doc, method=None):
     pe.party_type = "Employee"
     pe.party = doc.employee
 
-    # Mode of Payment (change field if different in your Employee Advance)
     pe.mode_of_payment = doc.mode_of_payment
 
-
-    bank_account =  frappe.db.get_value(
-        "Mode of Payment",
-        doc.mode_of_payment,
-        "default_bank_account",
-    )   
-    # Accounts (adjust according to your implementation)
+    # Accounts
     pe.paid_from = bank_account
     pe.paid_to = doc.advance_account
 
+    # Amount
     pe.paid_amount = doc.advance_amount
     pe.received_amount = doc.advance_amount
 
-    pe.append("references", {
-        "reference_doctype": "Employee Advance",
-        "reference_name": doc.name,
-        "allocated_amount": doc.advance_amount,
-        "total_amount": doc.advance_amount,
-        "outstanding_amount": doc.advance_amount,
-    })
+    # Reference
+    pe.append(
+        "references",
+        {
+            "reference_doctype": "Employee Advance",
+            "reference_name": doc.name,
+            "allocated_amount": doc.advance_amount,
+            "total_amount": doc.advance_amount,
+            "outstanding_amount": doc.advance_amount,
+        },
+    )
 
     pe.flags.ignore_permissions = True
     pe.insert()
+
+    # Submit if required
     # pe.submit()
 
-    frappe.msgprint(_("Payment Entry {0} created.").format(pe.name))
+    frappe.msgprint(
+        _("Payment Entry {0} created.").format(pe.name)
+    )
+    
